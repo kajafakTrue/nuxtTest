27ada368f9ed10ce4d8991dfdad70ad2516aa9b9
Podkovyrov Mikhail <podkovyrovmihail484@gmail.com>
2025-06-30 15:38:59 +0700

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <vue-mermaid-string :value="diagram" @node-click="handleNodeClick" />
+</template>
+
+<script>
+import endent from 'endent'
+
+export default {
+  computed: {
+    diagram() {
+      return endent`
+        classDiagram
+        Animal <|-- Duck
+        Animal <|-- Fish
+        Animal <|-- Zebra
+        class Duck {
+          +String beakColor
+          +swim()
+          +quack()
+        }
+        click Duck
+      `
+    }
+  },
+  methods: {
+    handleNodeClick(nodeId) {
+      if (nodeId === 'Duck') {
+        alert('Duck node clicked!')
+        // Your custom JS logic here
+      }
+    }
+  }
+}
+</script>
