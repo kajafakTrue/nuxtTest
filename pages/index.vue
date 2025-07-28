beb63d9d478d54ac6bfbdeaf09d5627923ba090a
Podkovyrov Mikhail <podkovyrovmihail484@gmail.com>
2025-07-28 21:54:01 +0700

    feat/feat clicks

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,38 @@
 <script setup lang="ts">
 import VueMermaidString from 'vue-mermaid-string'
 import endent from 'endent'
-import { computed } from 'vue'
+import { computed, onMounted, nextTick } from 'vue'
 
+const purchasesDiagram = computed(() => {
+  return `
+    classDiagram
+        class Purchase["Закупка"]
+        class Customer["Заказчик"]
+        class Supplier["Поставщик"]
+        
+        Purchase --> Customer
+        Purchase --> Supplier
+        
 
-function handleNodeClick(nodeId) {
-  console.log('Clicked node:', nodeId)
-}
-const diagram = computed(() => {
-  return endent`
-       classDiagram
-        Animal <|-- Duck
-        Animal <|-- Fish
-        Animal <|-- Zebra
-        class Duck {
-          +String beakColor
-          +swim()
-          +quack()
-        }  
-      `
+        click Purchase call handleDiagramClick("Purchase", "purchase")
+        click Customer call handleDiagramClick("Customer", "customer")
+        click Supplier call handleDiagramClick("Supplier", "supplier")
+  `
 })
+
+async function handleDiagramClick(nodeId, nodeType: string) {
+  console.log(await nodeId.srcElement.innerText)
+}
 </script>
-
 <template>
-  <VueMermaidString
 
-    :value="diagram"
+    <VueMermaidString
 
-    :options="{ theme: 'dark' }"
-    @node-click="handleNodeClick"
+    :value="purchasesDiagram"
+    @click="handleDiagramClick"
+
+    :options="{ theme: 'dark', class: {hideEmptyMembersBox: false, hideEmptyMembers: false, hideEmptyAttributes: false, hideEmptyMethods: false, hideEmptyProperties: false, hideEmptyConstructors: false, hideEmptyClasses: false, hideEmptyInterfaces: false, hideEmptyEnums: false, hideEmptyTypes: false, hideEmptyPackages: false, hideEmptyModules: false, hideEmptyNamespaces: false, hideEmptyObjects: false, hideEmptyFunctions: false, hideEmptyVariables: false, hideEmptyConstants: false, hideEmptyPropertiesBox: true
+  } }"
+
   />
-</template>
+</template>          
